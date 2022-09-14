2f23fcd44dec7907a3e2c351046c9c34b3640515
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-09-14 15:44:28 +0200

    Add example code for audioplayer

---
+++ components/AudioPlayerExample.js
@@ -0,0 +1,16 @@
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+
+const audioSrc =
+  "https://upload.wikimedia.org/wikipedia/commons/2/25/Dembow_Perreo%2C_Classic_Dembow%2C_and_Rich_Dembow_Beats.ogg";
+
+const AudioPlayerExample = () => (
+  <AudioPlayer
+    autoPlay
+    src={audioSrc}
+    onPlay={(e) => console.log("onPlay")}
+    // other props here
+  />
+);
+
+export default AudioPlayerExample;
