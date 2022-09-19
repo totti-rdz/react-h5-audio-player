50fd2d35eec9b1d75b658a5a834bfd6b4ed72d20
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-09-20 00:55:31 +0200

    remove Head from page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,6 @@ import AudioPlayerExample from "../components/AudioPlayerExample";
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-5 sm:px-20 text-center">
         <h1 className="text-6xl font-bold">hello world!</h1>
         <AudioPlayerExample />
