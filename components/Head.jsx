73917ae8b7c01416e8eda9fd5a10b5cf2817ab14
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-09-20 00:55:10 +0200

    Extract <head> to component

---
+++ components/Head.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NextHead from "next/head";
+
+const Head = () => {
+  return (
+    <NextHead>
+      <title>react-h5-audio-player</title>
+      <meta property="og:title" content="react-h5-audio-player" key="title" />
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  );
+};
+
+export default Head;
